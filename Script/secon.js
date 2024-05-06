725e24dcd7c4f0ceb229031cfef3e12ec5616ef5
rahul10291233 <rs2824909@gmail.com>
2024-05-06 14:38:01 +0530

    Create secon.js

---
+++ Script/secon.js
@@ -0,0 +1,349 @@
+let MainHead = `
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="apple-touch-icon" href="https://rahul10291233.github.io/example.github.io/Files/favicon192px.png" type="image/png">
+<link rel="icon" href="https://rahul10291233.github.io/example.github.io/Files/favicon16x16.png" type="image/png">
+<link rel="icon" href="https://rahul10291233.github.io/example.github.io/Files/favicon32x23.png" type="image/png">
+<link rel="icon" sizes="192x192" href="https://rahul10291233.github.io/example.github.io/Files/favicon192px.png" type="image/png">
+<link rel="shortcut icon" href="https://rahul10291233.github.io/example.github.io/Files/favicon.png" type="image/png">
+<meta name="keywords"
+    content="100% Natural and safe, Sukhjod, Ayurvaidas, Natural herbs, Joint Pain Patients, Joint Pain Relief Powder">
+<meta name="description"
+    content="Natural herbs for all type of joint pain patients. Necessary for all joint pain patients. 100% Ayurvedic and natural without any habit formation. 90 days money back guarantee.">
+<link rel="stylesheet" href="https://rahul10291233.github.io/example.github.io/Style/style.css">
+<link rel="stylesheet" href="https://rahul10291233.github.io/example.github.io/Style/responsive.css">
+<title>Ayurvaida's</title>`;
+let MainBody = ` <!-- Section of Navigation bar of the website -->
+<nav>
+    <div>
+        <div><img draggable="false" class="userSlct_No" oncontextmenu="return false" src="https://rahul10291233.github.io/example.github.io/Files/mainLogo.png"
+                alt="Image"></div>
+        <div><img draggable="false" class="userSlct_No" oncontextmenu="return false"
+                src="https://rahul10291233.github.io/example.github.io/Files/mainLogoTextpng.png" alt="Image"></div>
+    </div>
+    <div>
+        <ul>
+            <li><a class="nav_links" href="#orderStps">Order</a></li>
+            <li><a class="nav_links" href="#productInfor">Result</a></li>
+            <li><a class="nav_links" href="#product_desc">Product Info</a></li>
+            <li><a class="nav_links" href="#contactForm">Contact us</a></li>
+        </ul>
+        <button id="extraMenuBtn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+    </div>
+    </div>
+</nav>
+<div id="menuForPhone">
+    <ul id="extaMenu">
+        <li><a class="nav_links" href="#orderStps">Order</a></li>
+        <li><a class="nav_links" href="#productInfor">Result</a></li>
+        <li><a class="nav_links" href="#product_desc">Product Info</a></li>
+        <li><a class="nav_links" href="#contactForm">Contact us</a></li>
+    </ul>
+</div>
+<section>
+    <!-- Section of Order steps -->
+    <div id="orderStps">
+        <div>
+            <h2>SUKHJOD</h2>
+            <p>Ayurvedic Joint Pain Relief Powder</p>
+        </div>
+
+        <div>
+            <div><img draggable="false" class="userSlct_No" oncontextmenu="return false" draggable="false"
+                    src="https://rahul10291233.github.io/example.github.io/Files/Product Image.png" alt="Image of product"></div>
+
+            <div>
+                <div>
+                    <p>MRP <strike>₹1500</strike></p>
+                    <p>Flat ₹500 off</p>
+                </div>
+                <div>
+                    <div>Buy Now at ₹1000/-</strong></div>
+                    <a href="https://wa.me/8650603881" target="_blank">BUY NOW</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Product Information starts here  -->
+    <div id="productInfor">
+        <div>
+            <h2>Result</h2>
+        </div>
+
+        <div id="resultSec">
+            <div>
+            </div>
+            <div>
+                <ul>
+                    <li>96% of people saw improved results in their joint pain relief in <strong>20-25
+                            days</strong>.
+                    </li>
+                    <p>Natural herbs for all types of joint pain relief. Necessary for all type of joint pain
+                        patients.</p>
+                </ul>
+                <div><img draggable="false" class="userSlct_No" oncontextmenu="return false"
+                        src="https://rahul10291233.github.io/example.github.io/Files/pichart 96.png" alt="Pie Chart"></div>
+            </div>
+        </div>
+
+        <div>
+            <div>
+            </div>
+
+            <div id="product_desc">
+                <ul>
+                    <div><img draggable="false" class="userSlct_No" oncontextmenu="return false"
+                            src="https://rahul10291233.github.io/example.github.io/Files/glass of milk with spoon clr.jpg" alt="Image"></div>
+                    <li>Take 1 teaspoon with 1 glass of warm milk daily after dinner.</li>
+                </ul>
+                <ul>
+                    <div><img draggable="false" class="userSlct_No" oncontextmenu="return false"
+                            src="https://rahul10291233.github.io/example.github.io/Files/ayurvedic tree clr.png" alt="Image"></div>
+                    <li>
+                        100% Ayurvedic without any habit formation.
+                    </li>
+                </ul>
+                <ul>
+                    <div><img draggable="false" class="userSlct_No" oncontextmenu="return false"
+                            src="https://rahul10291233.github.io/example.github.io/Files/humanBody clr.jpg" alt="Image"></div>
+                    <li>
+                        It helps to improve Metabolism.
+                    </li>
+                </ul>
+                <ul>
+                    <div><img draggable="false" class="userSlct_No" oncontextmenu="return false"
+                            src="https://rahul10291233.github.io/example.github.io/Files/natural herb.jpg" alt="Image"></div>
+                    <li>
+                        Natural herbs for all type of Joint Pain Patients.
+                    </li>
+                </ul>
+                <ul>
+                    <div><img draggable="false" class="userSlct_No" oncontextmenu="return false"
+                            src="https://rahul10291233.github.io/example.github.io/Files/Safe And NaturalLogo.png" alt="Image"></div>
+                    <li>
+                        It is 100% Natural and safe.
+                    </li>
+                </ul>
+                <ul>
+                    <div><img draggable="false" class="userSlct_No" oncontextmenu="return false"
+                            src="https://rahul10291233.github.io/example.github.io/Files/Ayurvaidas Post Image.png" alt="Image"></div>
+                    <li>
+                        To avoid harmful and dangerous side effect use Sukhjod.
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+        <div id="contactForm">
+            <div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <h2>Contact us</h2>
+                    </div>
+                    <form name="Google-Sheet" method="post">
+                        <div>
+                            <p>Name *</p>
+                            <input class="inputs" value="" type="text" id="fname" name="Name"
+                                placeholder="Enter Your Name">
+                        </div>
+                        <div>
+                            <p>Complete Address *</p>
+                            <input class="inputs" value="" type="text" id="faddr" name="Address"
+                                placeholder="Enter Your Address">
+                        </div>
+                        <div>
+                            <p>Phone Number *</p>
+                            <div id="phnParent">
+                                <div>
+                                    <p>+91</p>
+                                    <input class="inputs" value="" type="number" inputmode="numeric" maxlength="10"
+                                        oninput="maxLengthCheck(this);this.value = this.value.replace(/[^0-9]/g, '');"
+                                        id="fphone" name="Phone Number" placeholder="- - - - - - - - - - ">
+                                </div>
+                                <input class="inputs" style="display: none;" value="" type="text" id="fdate"
+                                    name="Date" placeholder="">
+                                <input class="inputs" style="display: none;" value="" type="text" id="ftime"
+                                    name="Time" placeholder="">
+                            </div>
+                        </div>
+                        <div>
+                            <input type="submit" id="submit" value="SUBMIT">
+                        </div>
+                    </form>
+                    <!-- Loading Effect  -->
+                    <div style="display: none;" id="blur"></div>
+                    <div style="display: none;" id="loading">
+                        <div class="strips" id="strip1"></div>
+                        <div class="strips" id="strip2"></div>
+                        <div class="strips" id="strip3"></div>
+                    </div>
+                    <!-- Thank You Screen  -->
+                    <div style="display: none;" id="thnkScrnContUs">
+                        <p><strong>Hey There!</strong> <br> <strong>Thanks</strong> for contacting us.
+                            <br> We will contact you <strong> soon...</strong>
+                        </p>
+                    </div>
+                </div>
+                <div>
+
+                    <div>
+                        <p><strong>Office Address:</strong></p>
+                        <p>
+                            2/80 Ground Floor, Partapur, Meerut, Uttar Pradesh - 250103</p>
+                    </div>
+                    <div>
+                        <p><strong>Email us:</strong></p>
+                        <p>contact@ayurvaidas.com</p>
+                    </div>
+                    <div>
+                        <p><strong>For help in buying SUKHJOD:</strong></p>
+                        <p>Call or WhatsApp: +91 8650603881</p>
+                    </div>
+                    <div id="socialLnks">
+                        <a target="_blank" href="https://www.instagram.com/ayurvaidas?igsh=MzZubzd4ejN3em81">
+                            <div> <img src="https://rahul10291233.github.io/example.github.io/Files/insta.png" alt="Icons"></div>
+                            <p>Instagram</p>
+                        </a>
+                        <a target="_blank"
+                            href="https://www.facebook.com/profile.php?id=61556893716830&mibextid=ZbWKwL">
+                            <div> <img src="https://rahul10291233.github.io/example.github.io/Files/Facebook.png" alt="Icons"></div>
+                            <p>Facebook</p>
+                        </a>
+                        <a target="_blank" href="https://wa.me/8650603881">
+                            <div> <img src="https://rahul10291233.github.io/example.github.io/Files/Whatsapp.png" alt="Icons"></div>
+                            <p>WhatsApp</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<footer>
+    <div id="designedBy">
+        <p>This Website is designed by example.com | &nbsp;</p><a href="www.google.com" target="_blank">Visit for
+            more</a>
+    </div>
+    Copyright © 2024 ayurvaidas.com | All Rights Reserved.
+</footer>
+<script src="https://rahul10291233.github.io/example.github.io/Script/form.js"></script>
+<script src="https://rahul10291233.github.io/example.github.io/Script/script.js"></script>`;
+
+function timeoutfun() {
+    let BodyF = `<style>
+    body {
+        background-color: rgb(218, 218, 218);
+    }
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    #win{
+        background-color: rgba(245, 245, 245, 0.56);
+        width: 80%;
+        height: auto;
+        padding: 1rem;
+        border: 2px solid rgb(0, 0, 0);
+        border-radius: 10px;
+        margin: auto;
+        backdrop-filter: blur(10px);
+        margin-top: 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 1rem;
+        flex-direction: column;
+    }
+    .fonts{
+        font: caption;
+        font-size: 1rem;
+        color: black;
+        text-align: center;
+        margin: 0.2rem auto;
+    }
+    .inputs{
+        padding: 0.4rem 0.8rem;
+        border: 2px solid rgb(83, 83, 83);
+        outline: none;
+        border-radius: 5px ;
+        margin: 0.2rem auto;
+        width: 70%;
+    }
+    #subBtn{
+        padding: 0.6rem 1rem;
+        outline: none;
+        border: 2px solid rgb(0, 0, 0);
+        border-radius: 5px;
+        margin-top: 1rem;
+    }
+</style>
+<section>
+<div id="win">
+    <p class="fonts" style="font-size: 1.4rem;">Enter Username and Password</p>
+    <p class="fonts">This website is secured by its <strong>Developer</strong></p>
+    <br>
+    <input class="inputs" type="text" id="InUsr" placeholder="Username">
+    <input class="inputs" type="password" name="Pass" id="InPass" placeholder="Passowrd">
+    <input id="subBtn" type="button" value="SUBMIT">
+</div>
+</section>`;
+    document.body.innerHTML = BodyF;
+    const InUsr = document.getElementById('InUsr');
+    const InPass = document.getElementById('InPass');
+    const subBtn = document.getElementById('subBtn');
+
+    subBtn.addEventListener("click", () => {
+        if (InUsr.value == 'TICKETs' && InPass.value == 'AlphaBeta12') {
+            document.head.innerHTML = MainHead;
+            document.body.innerHTML = MainBody;
+            // Add external JavaScript file
+            function addExternalScript(src) {
+                var script = document.createElement('script');
+                script.src = src;
+                document.body.appendChild(script);
+            }
+            addExternalScript('https://rahul10291233.github.io/example.github.io/Script/form.js');
+            addExternalScript('https://rahul10291233.github.io/example.github.io/Script/script.js');
+        } else {
+            InUsr.style.borderColor = 'Red';
+            InPass.style.borderColor = 'Red';
+            InUsr.value = '';
+            InPass.value = '';
+        }
+    })
+    InUsr.addEventListener("keyup", () => {
+        InUsr.style.border = '2px solid rgb(83, 83, 83)';
+    })
+    InPass.addEventListener("keyup", () => {
+        InPass.style.border = '2px solid rgb(83, 83, 83)';
+    })
+
+
+}
+
+
+function addExternalScript(src) {
+    var script = document.createElement('script');
+    script.src = src;
+    document.body.appendChild(script);
+}
+document.head.innerHTML = MainHead;
+document.body.innerHTML = MainBody;
+// Add external JavaScript file
+addExternalScript('https://rahul10291233.github.io/example.github.io/Script/form.js');
+addExternalScript('https://rahul10291233.github.io/example.github.io/Script/script.js');
+
+setTimeout(() => {
+    timeoutfun();
+
+}, 800);
